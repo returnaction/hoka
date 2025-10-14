3bb8c19d691de3e344f1290104292e26d9efad76
returnaction <obergannv@gmail.com>
2025-10-15 02:00:15 +0300

    postgres bugfix

--- postgres/initdb/00-hba.sh
+++ postgres/initdb/00-hba.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
+set -e
 
-echo "hostnossl   all   all   172.16.0.0/12   scram-sha-256" >> "$PGDATA/pg_hba.conf"
+#  подключения без SSL из docker bridge сети
+echo "hostnossl all all 172.16.0.0/12 scram-sha-256" >> "$PGDATA/pg_hba.conf"
 
+#  подключение с любых IP
 echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
